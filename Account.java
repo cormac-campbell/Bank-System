a2c1d770dff207b760cdf00d3b4d096562efdf88
cormac-campbell <101560632+cormac-campbell@users.noreply.github.com>
2022-03-14 11:01:56 +0000

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,33 @@
+package implementation;
+
+public class Account 
+{
+	//variables
+		private int accountId;
+		private double balance;
+		
+		//setter methods
+		
+		public void setAccountId(int accountId)
+		{
+			this.accountId = accountId;
+		}
+		
+		public void setBalance(double balance)
+		{
+			this.balance = balance;
+		}
+		
+		//getter methods
+		
+		public int getAccountId()
+		{
+			return this.accountId;
+		}
+		
+		public double getBalance()
+		{
+			return this.balance;
+		} 
+		
+}
